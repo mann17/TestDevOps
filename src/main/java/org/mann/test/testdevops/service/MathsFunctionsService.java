8d569fc139e3873c459873176ff89ed8b6ef16ab
Manish Shukla <mann.gnit@gmail.com>
2021-04-04 12:56:58 +0530

    fixed isPrimeNumber issue

--- src/main/java/org/mann/test/testdevops/service/MathsFunctionsService.java
+++ src/main/java/org/mann/test/testdevops/service/MathsFunctionsService.java
@@ -1,8 +1,10 @@
 package org.mann.test.testdevops.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MathsFunctionsService {
 
     public String isEvenOddNumber(Integer number) {
@@ -17,14 +19,17 @@ public class MathsFunctionsService {
     }
 
     public String isPrimeNumber(Integer number) {
-        String message = String.format("Number %d is not a prime number", number);
+        String message = String.format("Number %d is a prime number", number);
 
-        if( number <= 1) {
+        if( number <= 0) {
             message = String.format("Number %d is not a valid number for evaluation", number);
+        } else if (number == 1) {
+            message = String.format("Number %d is a prime number", number);
         } else {
-            for (int i = 2; i <= number; i++) {
-                if(number % i == 0 && i == number) {
-                    message = String.format("Number %d is a prime number", number);
+            for (int i = 2; i < number; i++) {
+                if(number % i == 0) {
+                    log.info("Number <{}> is divisible by <{}> hence not a prime number", number, i);
+                    message = String.format("Number %d is not a prime number", number);
 
                     break;
                 }
