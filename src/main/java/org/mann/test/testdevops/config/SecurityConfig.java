cfeea7664377a082ea5ab711e785f90acbe047d8
Manish Shukla <mann.gnit@gmail.com>
2021-10-17 19:58:27 +0530

    enhanced app to support https

---
+++ src/main/java/org/mann/test/testdevops/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package org.mann.test.testdevops.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+}
